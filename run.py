8109865dc94c188f24104a8b535393376cb421f0
Ola Kveli <olakveli@yahoo.com>
2019-06-14 16:33:36 +0200

    Now the project can find pairs

--- run.py
+++ run.py
@@ -7,9 +7,17 @@ def throw_dice(amount_of_die: int):
 
 def find_pair(amount_of_paris: int, list_of_dice: list):
     sum_of_dice = 0
-    print(list_of_dice)
-    # TODO: Implement in the future
-    return sum_of_dice
+    for value in range(6, 0, -1):
+        counter = 0
+        for dice_number in list_of_dice:
+            if dice_number == value:
+                counter += 1
+                if counter >= amount_of_paris:
+                    sum_of_dice = max(dice_number * amount_of_paris, sum_of_dice)
+    if sum_of_dice is not 0:
+        return sum_of_dice
+    else:
+        print("Your roll did not get " + str(amount_of_paris) + " equal dice")
 
 
 def play_strict_order():
@@ -22,5 +30,6 @@ def play_loose_order():
 
 
 if __name__ == '__main__':
-    find_pair(2, throw_dice(6))
+    numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 5, 5]
+    print(find_pair(2, throw_dice(6)))
     exit()
