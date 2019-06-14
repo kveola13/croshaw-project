159a705e514168bc133e19ee23abb1de1e6becaf
Ola Kveli <olakveli@yahoo.com>
2019-06-14 18:48:08 +0200

    added functionality to exclude a number

--- run.py
+++ run.py
@@ -5,24 +5,41 @@ def throw_dice(amount_of_die: int):
     return [random.randint(1, 6) for _ in range(0, amount_of_die, 1)]
 
 
-def find_pair(amount_of_paris: int, list_of_dice: list):
+def find_pair(amount_of_paris: int, number_to_exclude: int, list_of_dice: list):
     sum_of_dice = 0
     for value in range(6, 0, -1):
         counter = 0
         for dice_number in list_of_dice:
-            if dice_number == value:
+            if dice_number == value and dice_number != number_to_exclude:
                 counter += 1
                 if counter >= amount_of_paris:
                     sum_of_dice = max(dice_number * amount_of_paris, sum_of_dice)
     if sum_of_dice is not 0:
-        return sum_of_dice
+        print(list_of_dice)
+        return sum_of_dice, int(sum_of_dice / amount_of_paris)
     else:
         print("Your roll did not get " + str(amount_of_paris) + " equal dice")
 
 
+def find_yahtzee(list_of_dice: list):
+    print(find_pair(6, 0, list_of_dice))
+
+
+def find_house():
+    try:
+        sum_of_dice, number_to_exclude = find_pair(3, 0, throw_dice(6))
+        print("Number to exclude: " + str(number_to_exclude))
+        print(find_pair(2, number_to_exclude, throw_dice(6)))
+    except TypeError:
+        print("You did not get 3 of the same")
+
+
 def play_strict_order():
     for index in range(1, 7, 1):
-        print(find_pair(index, throw_dice(6)))
+        print(find_pair(index, 0, throw_dice(6)))
+    find_house()
+    yahtzee_test_list = [5, 5, 5, 5, 5, 5]
+    find_yahtzee(yahtzee_test_list)
 
 
 def play_loose_order():
